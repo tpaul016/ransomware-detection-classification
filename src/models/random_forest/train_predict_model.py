a8d316e03031adc6e60f8f7f01f39c1ac925f6b5
Tristan Paul <tpaul016@uottawa.ca>
2020-04-28 14:58:12 -0400

    created random forest model

--- src/models/random_forest/train_predict_model.py
+++ src/models/random_forest/train_predict_model.py
@@ -8,8 +8,7 @@ import pickle
 df = pd.read_csv('../../features/processed_data_w_features.csv')
 labels = np.array(df['Label'])
 
-# TODO: dropping 'Tot Bytes' 'Tot Pkts' for now, have to go over their data
-df.drop(['Label', 'Dst IP', 'Src IP', 'Start Time', 'End Time', 'Tot Bytes', 'Tot Pkts'], axis=1, inplace=True)
+df.drop(['Label', 'Dst IP', 'Src IP', 'Start Time', 'End Time'], axis=1, inplace=True)
 
 feature_list = list(df.columns)
 features = np.array(df)
@@ -17,7 +16,6 @@ np.nan_to_num(features, copy=False)
 
 # splitting data in training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
-# train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 # analyzing shape
 print('Training Features Shape:', x_train.shape)
@@ -29,7 +27,7 @@ print('Testing Labels Shape:', y_test.shape)
 rf = RandomForestClassifier(n_estimators=1000, random_state=42)
 rf.fit(x_train, y_train)
 
-feature_imp = pd.Series(rf.feature_importances_,index=feature_list).sort_values(ascending=False)
+feature_imp = pd.Series(rf.feature_importances_, index=feature_list).sort_values(ascending=False)
 print(feature_imp)
 
 # saving serialized format of model
@@ -40,5 +38,11 @@ y_pred = rf.predict(x_test)
 
 # performance metrics
 accuracy = metrics.accuracy_score(y_test, y_pred)
-print("Accuracy: ",accuracy)
+print("Accuracy: ", accuracy)
+precision = metrics.precision_score(y_test, y_pred)
+print("Precision: ", precision)
+recall = metrics.recall_score(y_test, y_pred)
+print("Recall: ", recall)
+f1_score = metrics.f1_score(y_test, y_pred)
+print("F1-Score: ", f1_score)
 print(metrics.classification_report(y_test, y_pred))
